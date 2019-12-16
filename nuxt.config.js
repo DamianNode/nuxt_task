5399fc3bbd2821c2ca8c2519e6f22dc2278dfa96
damian.janus <damian.janus@imagene.me>
2019-12-16 23:25:47 +0100

    Point 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,16 @@
 export default {
-  loading: '~/components/loading.vue'
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'stylesheet', type: 'text/css', href: '/ProximaNova-Regular.otf' }
+    ]
+  },
+  loading: '~/components/loading.vue',
+  css: [
+    '~/assets/style.css'
+  ],
 }
